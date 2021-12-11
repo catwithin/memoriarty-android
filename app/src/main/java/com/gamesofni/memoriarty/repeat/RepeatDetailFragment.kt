b39d387e00b4a8e2b1f8da9e4fe25287aa0ab398
catwithin <catwithin@protonmail.com>
2021-12-11 14:55:25 +0100

    Add repeat detail fragment

---
+++ app/src/main/java/com/gamesofni/memoriarty/repeat/RepeatDetailFragment.kt
@@ -0,0 +1,20 @@
+package com.gamesofni.memoriarty.repeat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gamesofni.memoriarty.R
+
+
+class RepeatDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_repeat_detail, container, false)
+    }
+
+}
