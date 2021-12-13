46acd4885106c8b89ea4169c4dbef5d13221b5f9
catwithin <catwithin@protonmail.com>
2021-12-13 21:53:44 +0100

    up button navigation

--- app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
+++ app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
@@ -2,11 +2,23 @@ package com.gamesofni.memoriarty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.overviewFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
 //        setContentView(R.layout.activity_repeat_detail)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.overviewFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp()
     }
 }
