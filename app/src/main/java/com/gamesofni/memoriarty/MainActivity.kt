7da897f4dd4c718df396db49813b1e906b3bfc9b
catwithin <catwithin@protonmail.com>
2021-12-20 11:49:35 +0100

    add navigation drawer

--- app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
+++ app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
@@ -2,18 +2,24 @@ package com.gamesofni.memoriarty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.gamesofni.memoriarty.databinding.ActivityMainBinding
+import com.gamesofni.memoriarty.databinding.TodayFragmentOverviewBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.overviewFragment) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this,navController)
-//        setContentView(R.layout.activity_repeat_detail)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
