c70fdf4a9323ccf17d6e797625aa6cef5c74a5c9
catwithin <catwithin@protonmail.com>
2021-12-20 15:13:57 +0100

    add share button and start implicit activity

--- app/src/main/java/com/gamesofni/memoriarty/repeat/RepeatDetailFragment.kt
+++ app/src/main/java/com/gamesofni/memoriarty/repeat/RepeatDetailFragment.kt
@@ -1,10 +1,9 @@
 package com.gamesofni.memoriarty.repeat
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import com.gamesofni.memoriarty.R
 
@@ -17,7 +16,40 @@ class RepeatDetailFragment : Fragment() {
     ): View? {
         val args = RepeatDetailFragmentArgs.fromBundle(requireArguments())
         Toast.makeText(context, "Description: ${args.description}", Toast.LENGTH_LONG).show()
+
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_repeat_detail, container, false)
     }
 
+
+    private fun getShareIntent() : Intent {
+        val args = RepeatDetailFragmentArgs.fromBundle(requireArguments())
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_memoriarty_repeat, args
+                .description))
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    // Showing the Share Menu Item Dynamically
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+        if(getShareIntent().resolveActivity(requireActivity().packageManager)==null){
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    // Sharing from the Menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
