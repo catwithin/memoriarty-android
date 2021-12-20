8b7fce28fae15fa8a23e1ca48ed0a6af78df760e
catwithin <catwithin@protonmail.com>
2021-12-20 11:56:11 +0100

    add drawer button

--- app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
+++ app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gamesofni.memoriarty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.gamesofni.memoriarty.databinding.ActivityMainBinding
@@ -10,14 +11,17 @@ import com.gamesofni.memoriarty.databinding.TodayFragmentOverviewBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        drawerLayout = binding.drawerLayout
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.overviewFragment) as NavHostFragment
         val navController = navHostFragment.navController
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
 
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
@@ -25,6 +29,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.overviewFragment) as NavHostFragment
         val navController = navHostFragment.navController
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
