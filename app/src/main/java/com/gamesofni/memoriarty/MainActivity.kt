f27849c99886353a59db4314c4a35b26f3787082
catwithin <catwithin@protonmail.com>
2021-12-24 10:20:24 +0100

    add Timer class

--- app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
+++ app/src/main/java/com/gamesofni/memoriarty/MainActivity.kt
@@ -7,11 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.gamesofni.memoriarty.databinding.ActivityMainBinding
-import com.gamesofni.memoriarty.databinding.TodayFragmentOverviewBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
 
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+        timer = Timer()
     }
 
     override fun onSupportNavigateUp(): Boolean {
